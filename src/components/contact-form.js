a6229e8e703e373ad495b0e183aa212511d83e87
mattrbanks <banks.matt81@gmail.com>
2021-02-05 18:29:20 -0500

    Developed the contact-form.js component. It is almost done and will be handled through Netlify.

--- src/components/contact-form.js
+++ src/components/contact-form.js
@@ -1,7 +1,160 @@
 import React from "react";
 
 const ContactForm = () => {
-  return <div>Hello from contactform</div>;
+  const [file, setFile] = React.useState(null);
+  const [name, setName] = React.useState("");
+  const [email, setEmail] = React.useState("");
+  const [message, setMessage] = React.useState("");
+  const [phone, setPhone] = React.useState(""); //needs form field
+  const [subject, setSubject] = React.useState("");
+  const [image, setImage] = React.useState("");
+
+  function handleImageChange(event) {
+    if (!event.target.files[0]) {
+      setFile(file);
+    } else {
+      setFile(URL.createObjectURL(event.target.files[0]));
+      setImage(file);
+    }
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    //Do I need to do something else here?
+  }
+
+  const contactInfo = {
+    //Will I use this variable with Netlify?
+    name,
+    email,
+    message,
+    phone,
+    subject,
+    image, // This is currently optional and will be an empty string if the user does not attach an image of the job.
+  };
+
+  return (
+    <div className="flex justify-center my-6">
+      <form className="w-full max-w-lg" onSubmit={handleSubmit}>
+        <div className="flex flex-wrap -mx-3 mb-6">
+          <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+            <label
+              className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+              htmlFor="full-name"
+            >
+              Full Name
+            </label>
+            <input
+              className="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+              name="full-name"
+              type="text"
+              placeholder="full name"
+              required
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            ></input>
+          </div>
+          <div className="w-full md:w-1/2 px-3">
+            <label
+              className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+              htmlFor="subject"
+            >
+              Subject
+            </label>
+            <input
+              className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+              name="subject"
+              type="text"
+              placeholder="subject"
+              required
+              value={subject}
+              onChange={(e) => setSubject(e.target.value)}
+            ></input>
+          </div>
+        </div>
+        <div className="flex flex-wrap -mx-3 mb-6">
+          <div className="w-full px-3">
+            <label
+              className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+              htmlFor="email"
+            >
+              Email
+            </label>
+            <input
+              className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+              name="email"
+              type="email"
+              placeholder="youraddress@email.com"
+              required
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            ></input>
+          </div>
+        </div>
+        <div className="flex flex-wrap -mx-3 mb-6">
+          <div className="w-full px-3">
+            <label
+              className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+              htmlFor="message"
+            >
+              Message
+            </label>
+            <textarea
+              className=" no-resize appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 h-48 resize-none"
+              name="message"
+              placeholder="type your message..."
+              required
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+            ></textarea>
+          </div>
+        </div>
+        <div className="flex">
+          <div>
+            <p className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
+              Attach an image of the job you want done
+            </p>
+            <div className="flex items-center justify-center bg-grey-lighter">
+              <label className="w-64 flex flex-col items-center px-4 py-6 bg-white text-blue-800 rounded-lg shadow-lg tracking-wide uppercase border border-blue-800 cursor-pointer hover:bg-blue-800 hover:text-white">
+                <svg
+                  className="w-8 h-8"
+                  fill="currentColor"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                >
+                  <path d="M16.88 9.1A4 4 0 0 1 16 17H5a5 5 0 0 1-1-9.9V7a3 3 0 0 1 4.52-2.59A4.98 4.98 0 0 1 17 8c0 .38-.04.74-.12 1.1zM11 11h3l-4-4-4 4h3v3h2v-3z" />
+                </svg>
+                <span className="mt-2 text-base leading-normal">
+                  Select a file
+                </span>
+                <input
+                  type="file"
+                  onChange={handleImageChange}
+                  className="hidden"
+                />
+              </label>
+            </div>
+          </div>
+          <div className="mx-6">
+            <div>
+              <img src={file} />
+            </div>
+          </div>
+        </div>
+        <div className="md:flex md:items-center">
+          <div className="md:w-1/3">
+            <button
+              type="button"
+              className="border border-indigo-500 bg-indigo-500 text-white rounded-md px-4 py-2 m-2 transition duration-500 ease select-none hover:bg-indigo-600 focus:outline-none focus:shadow-outline"
+            >
+              Send
+            </button>
+          </div>
+          <div className="md:w-2/3"></div>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default ContactForm;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,11 @@
 import * as React from "react";
 import Layout from "../components/layout";
+import Footer from "../components/footer";
+import ContactForm from "../components/contact-form";
+import Testimonials from "../components/testimonials";
+import Gallery from "../components/gallery";
+import Services from "./services";
+
 //import SEO from "../components/seo";
 //import ImageReusable from "../components/imageReusable";
 
@@ -16,11 +22,21 @@ const IndexPage = () => {
           rich's property maintenance llc is a place where all your home
           maintenance needs.
         </p>
-        <div className="w-full h-screen bg-black"></div>
-        <div className="w-full h-screen bg-white"></div>
-        <div className="w-full h-screen bg-black"></div>
-        <div className="w-full h-screen bg-white"></div>
-        <div className="w-full h-screen bg-black"></div>
+        <div className="w-full h-screen border">
+          <Services />
+        </div>
+        <div className="w-full h-screen border">
+          <Gallery />
+        </div>
+        <div className="w-full h-screen border">
+          <Testimonials />
+        </div>
+        <div className="w-full h-screen border">
+          <ContactForm />
+        </div>
+        <div className="w-full h-64 border px-4 py-4">
+          <Footer />
+        </div>
       </main>
     </Layout>
   );
