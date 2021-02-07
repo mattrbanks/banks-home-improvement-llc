c54107916cbd9ad03c0428da983895d5df31e8be
mattrbanks <banks.matt81@gmail.com>
2021-02-07 13:40:22 -0500

    Changed button type on the contact form to submit.

--- src/components/contact-form.js
+++ src/components/contact-form.js
@@ -182,7 +182,7 @@ const ContactForm = () => {
         <div className="md:flex md:items-center">
           <div className="md:w-1/3">
             <button
-              type="button"
+              type="submit"
               className="border border-indigo-500 bg-indigo-500 text-white rounded-md px-4 py-2 m-2 transition duration-500 ease select-none hover:bg-indigo-600 focus:outline-none focus:shadow-outline"
             >
               Send
